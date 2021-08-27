c13fc0572526b3754b9f43fd55cf395a8208841c
Jose Flores Vacio <josefvacio@gmail.com>
2021-08-27 15:28:01 -0700

    Se termino el menu hamburguesa y funciona adecuadamente

--- dev/js/index.js
+++ dev/js/index.js
@@ -1,3 +1,45 @@
+
+const navContainer = document.getElementById('navigation');
+
+const openDefaultOption = document.querySelector('.list-container');
+
+const navManagment = (defaultOption) => {
+    let option = defaultOption;
+
+    function changeOption (newOption) {
+        option.removeAttribute('open')
+        option = newOption;
+    }
+    return changeOption;
+}
+
+const change = navManagment(openDefaultOption);
+
+navContainer.addEventListener('click',  (e) => {
+    const option = e.target.parentElement;
+    if(!option.open) {
+        change(option);
+    }
+    
+})
+
+const buttonMenu = document.getElementById('buttonMenu');
+
+buttonMenu.addEventListener('click', (e) => {
+    const menu = e.target;
+    const navHeader = menu.previousElementSibling;
+    if(menu.classList.contains('hamburguer')) {
+        menu.classList.replace('hamburguer', 'close');
+        navHeader.classList.replace('close', 'open');
+
+
+    } else if (menu.classList.contains('close')) {
+        menu.classList.replace('close', 'hamburguer');
+        navHeader.classList.replace('open', 'close');
+    }
+})
+
+
 const details = document.querySelector('.footer').querySelectorAll('.list-container');
 
 const arrayDetails = [...details]
