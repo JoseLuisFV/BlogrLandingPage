63177a8f33011a03ce6addf7e7aa4bdcd8f79b0e
Jose Flores Vacio <josefvacio@gmail.com>
2021-09-06 17:26:26 -0700

    Corrgiendo un poco el funcionamiento de los menus

--- dev/js/index.js
+++ dev/js/index.js
@@ -16,7 +16,15 @@ const navManagment = (defaultOption) => {
 const change = navManagment(openDefaultOption);
 
 navContainer.addEventListener('click',  (e) => {
-    const option = e.target.parentElement;
+    let option;
+    if (e.target.tagName === "SPAN") {
+        option = e.target.parentElement.parentElement
+    } else if (e.target.tagName === "SUMMARY") {
+        option = e.target.parentElement;
+    } else {
+        option = e.target
+    }
+
     if(!option.open) {
         change(option);
     }
@@ -26,6 +34,7 @@ navContainer.addEventListener('click',  (e) => {
 const buttonMenu = document.getElementById('buttonMenu');
 
 buttonMenu.addEventListener('click', (e) => {
+    
     const menu = e.target;
     const navHeader = menu.previousElementSibling;
     if(menu.classList.contains('hamburguer')) {
